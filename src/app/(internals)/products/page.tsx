4d454d894ddd57e755bd15876593f6dd83342333
Luan Santos <luan.santos@gbtsolutions.pt>
2025-01-30 12:25:01 +0000

    folder transparent to path

--- src/app/(internals)/products/page.tsx
+++ src/app/(internals)/products/page.tsx
@@ -4,7 +4,7 @@ export default function page() {
     return (
         <div>
             <h1>Products Page</h1>
-            <Link href="/products/details">Redirect to Details</Link>
+            <Link href="/src/app/(internals)/products/details">Redirect to Details</Link>
         </div>
     );
 }
